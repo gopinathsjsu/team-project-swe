ad6fa7f61af913dbd8324c12740998d0cb0d1b6f
Rashmi Sharma <rashmisharma489@gmail.com>
2023-09-21 09:34:35 -0700

    rs-project-setup: set up project, added sample endpoint and html, added links to README

---
+++ src/main/java/com/team_swe_project/TeamSweProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team_swe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSweProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamSweProjectApplication.class, args);
+	}
+
+}
