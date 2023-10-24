d6b3847d04c2f09a23ce976d91d5280d20502556
vanivineela-aremanda <vanivineela.aremanda@sjsu.edu>
2023-10-23 21:49:45 -0700

    adding registartion form pages and Reg.js and modified app.js with reg router]

---
+++ frontend/src/pages/Reg.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import RegForm from '../components/RegForm';
+const Registartion = () => {
+  return (
+    <><div>Registartion Form </div><div><RegForm /></div></>
+  );
+}
+
+export default Registartion;
