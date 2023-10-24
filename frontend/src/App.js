11f1f3dbe835a593b06e23a675a8acd1d286adaa
vanivineela-aremanda <vanivineela.aremanda@sjsu.edu>
2023-10-23 22:11:07 -0700

    Revert "adding registartion form pages and Reg.js and modified app.js with re…"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Reg from './pages/Reg';
+
 const App = () => {
   return (
 
     <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<Home />} /> 
-        <Route path="/reg" exact element={<Reg />} />
+        <Route path="/" exact element={<Home />} />
       </Routes>
     </BrowserRouter>
 
