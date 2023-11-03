982a55db1be6d42a02c0c9ad57bff646985abccf
Rashmi Sharma <rashmisharma489@gmail.com>
2023-11-03 09:38:12 -0700

    added logic to create user upon registration

--- backend/src/main/java/com/controllers/UserController.java
+++ backend/src/main/java/com/controllers/UserController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.entities.Users;
 import com.services.UsersService;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
