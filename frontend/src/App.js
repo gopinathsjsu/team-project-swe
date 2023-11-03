5c6202103d2ed01b0f260b30729a1184fcc7730e
vanivineela-aremanda <vanivineela.aremanda@sjsu.edu>
2023-11-02 22:59:31 -0700

    Register_Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import MemberDashboard from './pages/MemberDashboard';
-
+import Register from './pages/Register';
+import Login from './pages/Login';
 const App = () => {
   return (
 
@@ -10,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/member" exact element={<MemberDashboard/>}/>
+        <Route path="/register" exact element={<Register/>}/>
+        <Route path="/login" exact element={<Login/>}/>
       </Routes>
     </BrowserRouter>
 
