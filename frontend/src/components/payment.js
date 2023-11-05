c319c4e8c44aafc97fdc0c5457b9311406f36719
vanivineela-aremanda <vanivineela.aremanda@sjsu.edu>
2023-11-05 12:15:59 -0800

    created Membership Selection pages

---
+++ frontend/src/components/payment.js
@@ -0,0 +1,11 @@
+//Created a dummy payment page 
+//To check if MembershipSelectionForm.js is getting redirected to this page or not.
+import React from 'react'
+
+const payment = () => {
+  return (
+    <div>PAYMENT PAGE</div>
+  )
+}
+
+export default payment;
