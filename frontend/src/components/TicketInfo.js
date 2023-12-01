1117bbeca3ab08caee39540894e49e9aa994baf7
vanivineela-aremanda <vanivineela.aremanda@sjsu.edu>
2023-12-01 11:48:31 -0800

    ticket info page

---
+++ frontend/src/components/TicketInfo.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const TicketInfoPage = () => {
+  const [ticketData, setTicketData] = useState({});
+
+  //   useEffect(() => {
+  //     const id = 5;
+  //     async function getTicket() {
+  //       await axios
+  //         .get(`http://localhost:8080/api/tickets/getTicket?ticketId=${id}`)
+  //         .then((res) => setTicketData(res.data))
+  //         .catch((error) => console.error("Error fetching ticket data:", error));
+  //       console.log(ticketData);
+  //     }
+  //     getTicket();
+  //   }, []);
+  useEffect(() => {
+    const fetchTicketData = async () => {
+      try {
+        const response = await axios.get("/api/tickets/getTicket", {
+          params: {
+            ticketId: 6,
+          },
+        });
+        setTicketData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching ticket data:", error);
+      }
+    };
+
+    fetchTicketData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Booked Movie Tickets</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Ticket ID</th>
+            {/* <th>Show Time</th> */}
+            <th>Booking Date</th>
+            <th>Status</th>
+            {/* <th>Theater ID</th> */}
+            <th>Price</th>
+            <th>Seat Assignment</th>
+            {/* <th>Multiplex ID</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {/* {ticketData.map((ticket) => ( */}
+          {ticketData && (
+            <tr key={ticketData.ticketId}>
+              <td>{ticketData.ticketId}</td>
+              {/* <td>{ticketData.showtime.time}</td> */}
+              <td>{ticketData.bookingDate}</td>
+              <td>{ticketData.status}</td>
+              {/* <td>{ticketData.theaterId}</td> */}
+              <td>{ticketData.price}</td>
+              <td>{ticketData.seatAssignment}</td>
+              {/* <td>{ticketData.mulitplexId}</td> */}
+            </tr>
+          )}
+          {/* ))} */}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TicketInfoPage;
