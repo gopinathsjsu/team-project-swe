9126f335009021b9162ac24ce1ca20b565aac1a2
Rashmi Sharma <rashmisharma489@gmail.com>
2023-12-03 13:01:52 -0800

    finalizing admin analytics

--- frontend/src/pages/admin/AnalyticsDashboard.js
+++ frontend/src/pages/admin/AnalyticsDashboard.js
@@ -24,11 +24,49 @@ const AnalyticsDashboard = () => {
   const [selectedMultiplex, setSelectedMultiplex] = useState({});
   const [theaterOptions, setTheaterOptions] = useState([]);
   const [movieOptions, setMovieOptions] = useState([]);
-  const [selectedTheater, setSelectedTheater] = useState('');
-  const [selectedMovie, setSelectedMovie] = useState('');
+  const [selectedTheater, setSelectedTheater] = useState(null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
   const [selectedTimePeriod, setSelectedTimePeriod] = useState('30');
   const [groupBy, setGroupBy] = useState('');
-  const [theaterOccupancy, setTheaterOccupancy] = useState(null);
+
+  const [occupancyData, setOccupancyData] = useState(null);
+
+  useEffect(() => {
+    const fetchOccupancyData = async () => {
+      try {
+        let endpoint;
+        let selectedId;
+
+        if (groupBy === 'location' && selectedTheater.theaterId) {
+          endpoint = `/api/theaters/${selectedTheater.theaterId}/occupancyByLocation`;
+          selectedId = selectedTheater.theaterId;
+        } else if (groupBy === 'movie' && selectedMovie.movieId) {
+          endpoint = `/api/theaters/occupancyByMovie`;
+          selectedId = selectedMovie.movieId;
+        }
+
+        let response;
+        if (endpoint && selectedId) {
+          if (groupBy === 'location') {
+            response = await axios.get(`http://localhost:8080${endpoint}?days=${parseInt(selectedTimePeriod)}`);
+          } else {
+            response = await axios.get(`http://localhost:8080${endpoint}?days=${parseInt(selectedTimePeriod)}&movieId=${selectedId}`);
+          }
+          setOccupancyData(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching occupancy data:', error);
+      }
+    };
+
+    console.log("GROUP BY:", groupBy, "SELECTED THEATER:", selectedTheater, "SELECTED MOVIE:", selectedMovie, "SELECTED TIME PERIOD:", selectedTimePeriod);
+
+    if (groupBy && (selectedTheater || selectedMovie) && selectedTimePeriod) {
+      fetchOccupancyData();
+    }
+  }, [groupBy, selectedTheater, selectedMovie, selectedTimePeriod]);
+
+
 
   const handleLocationChange = (location) => {
     setSelectedLocation(location);
@@ -38,9 +76,32 @@ const AnalyticsDashboard = () => {
     setSelectedMultiplex(multiplex);
   };
 
+  const handleTheaterChange = (e) => {
+    const selectedTheaterId = +e.target.value;
+    const selectedTheater = theaterOptions.find(theater => {
+      return theater.theaterId === selectedTheaterId;
+    });
+    setSelectedTheater(selectedTheater || null);
+  };
+  
+  const handleMovieChange = (e) => {
+    const selectedMovieId = +e.target.value;
+    const selectedMovie = movieOptions.find(movie => movie.movieId === selectedMovieId);
+    console.log('Selected Movie:', selectedMovie);
+    setSelectedMovie(selectedMovie || null);
+  };
+
+  const handleTimePeriodChange = (e) => {
+    console.log('Selected Time Period:', e.target.value);
+    setSelectedTimePeriod(e.target.value);
+  };
+
+
+
   useEffect(() => {
     const getTheatersByMultiplexId = async () => {
       try {
+        setSelectedTheater(null);
         const response = await axios.get(`http://localhost:8080/api/theaters/getTheatersByMultiplexId/${selectedMultiplex.multiplexId}`);
         setTheaterOptions(response.data);
       } catch (error) {
@@ -50,6 +111,7 @@ const AnalyticsDashboard = () => {
 
     const getMoviesByLocationId = async () => {
       try {
+        setSelectedMovie(null);
         const response = await axios.get(`http://localhost:8080/api/movies/multiplex/${selectedMultiplex.multiplexId}`);
         setMovieOptions(response.data);
       } catch (error) {
@@ -62,47 +124,6 @@ const AnalyticsDashboard = () => {
       getMoviesByLocationId();
     }
   }, [selectedMultiplex]);
-
-  useEffect(() => {
-    const getTheaterOccupancyByLocation = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8080/api/theaters/${selectedTheater}/occupancyByLocation`, {
-          params: {
-            days: parseInt(selectedTimePeriod),
-          },
-        });
-        console.log('PRINTING OCCUPANCY:', response.data);
-        setTheaterOccupancy(response.data);
-      } catch (error) {
-        console.error('Error fetching theater occupancy by location:', error);
-      }
-    };
-
-    if (selectedLocation.locationId && selectedTheater.theaterId && selectedTimePeriod) {
-      getTheaterOccupancyByLocation();
-    }
-  }, [selectedLocation, selectedTheater, selectedTimePeriod]);
-
-  useEffect(() => {
-    const getTheaterOccupancyByMovie = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8080/api/theaters/occupancyByMovie`, {
-          params: {
-            movieId: selectedMovie.movieId,
-            days: parseInt(selectedTimePeriod),
-          },
-        });
-        console.log('PRINTING OCCUPANCY:', response.data);
-        setTheaterOccupancy(response.data);
-      } catch (error) {
-        console.error('Error fetching theater occupancy by movie:', error);
-      }
-    };
-
-    if (selectedLocation.locationId && selectedTheater.theaterId && selectedTimePeriod) {
-      getTheaterOccupancyByMovie();
-    }
-  }, [selectedLocation, selectedTheater, selectedMovie, selectedTimePeriod]);
 
   return (
     <div className="p-6">
@@ -132,10 +153,7 @@ const AnalyticsDashboard = () => {
               <InputLabel>Time Period</InputLabel>
               <Select
                 value={selectedTimePeriod}
-                onChange={(e) => {
-                  console.log('selected time period:', e.target.value);
-                  setSelectedTimePeriod(e.target.value);
-                }}
+                onChange={handleTimePeriodChange}
               >
                 <MenuItem value="30">Last 30 days</MenuItem>
                 <MenuItem value="60">Last 60 days</MenuItem>
@@ -143,18 +161,15 @@ const AnalyticsDashboard = () => {
               </Select>
             </FormControl>
 
-            {groupBy === 'location' && (
-              <>
-                <div className="mt-3">
+            <div className="mt-3">
+              {groupBy === 'location' && (
+                <>
                   <Typography variant="h6">Theater Occupancy by Location</Typography>
                   {theaterOptions && theaterOptions.length > 0 && (
                     <FormControl>
                       <NativeSelect
-                        value=""
-                        onChange={(e) => {
-                          console.log('selected theater:', e.target.value);
-                          setSelectedTheater(theaterOptions.find((theater) => theater.theaterId === e.target.value) || {});
-                        }}
+                        value={selectedTheater ? selectedTheater.theaterId : ""}
+                        onChange={handleTheaterChange}
                       >
                         <option value="" disabled>
                           Select a theater
@@ -167,68 +182,44 @@ const AnalyticsDashboard = () => {
                       </NativeSelect>
                     </FormControl>
                   )}
-                </div>
-                {selectedTheater && (
-                  <>
-                    {theaterOccupancy ? (
-                      <Typography>
-                        {selectedTheater.theaterName} Occupancy: {theaterOccupancy}
-                      </Typography>
-                    ) : (
-                      <Typography>
-                        {selectedTheater.theaterName} Occupancy: 0
-                      </Typography>
-                    )}
-                  </>
-                  
-                )}
-                
-              </>
-            )}
+                </>
+              )}
 
-            {groupBy === 'movie' && (
-              <>
-                <div className="mt-3">
+              {groupBy === 'movie' && (
+                <>
                   <Typography variant="h6">Theater Occupancy by Movie</Typography>
                   {movieOptions && movieOptions.length > 0 && (
                     <FormControl>
-                      <NativeSelect
-                        value=""
-                        onChange={(e) => {
-                          console.log('selected movie:', e.target.value);
-                          setSelectedMovie(movieOptions.find((movie) => movie.movieId === e.target.value) || {});
-                        }}
-                      >
-                        <option value="" disabled>
-                          Select a movie
+                    <NativeSelect
+                      value={selectedMovie ? selectedMovie.movieId : ""}
+                      onChange={handleMovieChange}
+                    >
+                      <option value="" disabled>
+                        Select a movie
+                      </option>
+                      {movieOptions.map((movie) => (
+                        <option key={movie.movieId} value={movie.movieId}>
+                          {movie.title}
                         </option>
-                        {movieOptions.map((movie) => (
-                          <option key={movie.movieId} value={movie.movieId}>
-                            {movie.title}
-                          </option>
-                        ))}
-                      </NativeSelect>
-                    </FormControl>
+                      ))}
+                    </NativeSelect>
+                  </FormControl>
                   )}
-                </div>
-                {selectedMovie && (
-                  <>
-                    {theaterOccupancy ? (
-                      <Typography>
-                        {selectedTheater.theaterName} Occupancy: {theaterOccupancy}
-                      </Typography>
-                    ) : (
-                      <Typography>
-                        {selectedTheater.theaterName} Occupancy: 0
-                      </Typography>
-                    )}
-                  </>
-                  
-                )}
-              </>
-            )}
+                </>
+              )}
+            </div>
           </CardContent>
         </Card>
+
+      )}
+
+      {occupancyData !== null && (selectedTheater || selectedMovie) && (
+        <Typography>
+          {console.log('Selected Theater:', selectedTheater)}
+          {groupBy === 'location'
+            ? `${selectedTheater ? selectedTheater.theaterName : 'Unknown Theater'} Occupancy: ${occupancyData}`
+            : `${selectedMovie ? selectedMovie.title : 'Unknown Movie'} Occupancy: ${occupancyData}`}
+        </Typography>
       )}
     </div>
   );
@@ -236,3 +227,46 @@ const AnalyticsDashboard = () => {
 
 export default AnalyticsDashboard;
 
+
+
+  // useEffect(() => {
+  //   const getTheaterOccupancyByLocation = async () => {
+  //     try {
+  //       const response = await axios.get(`http://localhost:8080/api/theaters/${selectedTheater.theaterId}/occupancyByLocation`, {
+  //         params: {
+  //           days: parseInt(selectedTimePeriod),
+  //         },
+  //       });
+  //       console.log('PRINTING OCCUPANCY:', response.data);
+  //       setTheaterOccupancy(response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching theater occupancy by location:', error);
+  //     }
+  //   };
+
+  //   if (selectedTheater.theaterId && selectedTimePeriod) {
+  //     console.log("REACHED HERE");
+  //     getTheaterOccupancyByLocation();
+  //   }
+  // }, [selectedTheater, selectedTimePeriod]);
+
+  // useEffect(() => {
+  //   const getTheaterOccupancyByMovie = async () => {
+  //     try {
+  //       const response = await axios.get(`http://localhost:8080/api/theaters/occupancyByMovie`, {
+  //         params: {
+  //           movieId: selectedMovie.movieId,
+  //           days: parseInt(selectedTimePeriod),
+  //         },
+  //       });
+  //       console.log('PRINTING OCCUPANCY:', response.data);
+  //       setTheaterOccupancy(response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching theater occupancy by movie:', error);
+  //     }
+  //   };
+
+  //   if (selectedMovie.movieId && selectedTimePeriod) {
+  //     getTheaterOccupancyByMovie();
+  //   }
+  // }, [selectedMovie, selectedTimePeriod]);
