165fa752cba8f6da9b2f94ea33d88d561f7b659e
nityasunkara <lakshmisrinitya.sunkara@sjsu.edu>
2023-12-04 09:47:44 -0800

    changed logo

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from '@mui/material/Button';
 import { Link, Outlet } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
-
+import logo from "../assets/logo.png"
 const NavBar = ({ isAdmin }) => {
   const navigate = useNavigate();
   const handleRegister = () =>{
@@ -14,7 +14,7 @@ const NavBar = ({ isAdmin }) => {
   return (
     <div className='bg-[#151515] flex justify-between py-[10px] sticky top-0'>
       <div className='text-[#E9DCC9] flex items-center gap-[5px] font-extrabold text-[26px] italic  bg-[#151515]'>
-        <Link to={{pathname:'/'}}><img alt='imdblogo' className='h-[80px]  ml-[40px]' src="https://i.pinimg.com/736x/ea/8d/11/ea8d11f1ffc6355b8a440106ce61d0f3.jpg"/></Link>
+        <Link to={{pathname:'/'}}><img alt='imdblogo' className='h-[40px]  ml-[40px]' src={logo}/></Link>
       <h3>Book My Movie</h3>
         </div>
         <div className='flex gap-[40px] mr-[40px] '>
