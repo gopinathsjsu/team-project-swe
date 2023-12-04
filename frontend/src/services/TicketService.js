3594eb9b2aaef98d14cbeb000ea40b8ad5212f6d
tanupa <tanupa01@gmail.com>
2023-12-03 16:59:22 -0800

    finished membershippage, updated paymentpage, updated TicketService.js, updated App.js

--- frontend/src/services/TicketService.js
+++ frontend/src/services/TicketService.js
@@ -4,21 +4,22 @@ import authHeader from './auth/auth-header';
 const TICKETS_BASE_URL = 'http://localhost:8080/api/tickets';
 
 class TicketsService {
-
     async getTicket(ticketId) {
         try {
             const response = await axios.get(`${TICKETS_BASE_URL}/getTicket`, {
                 params: {
                     ticketId
                 },
+                headers: authHeader()
             });
             return response.data;
         } catch (error) {
             console.error('Error fetching ticket:', error);
-            throw error; 
+            return null;
         }
-    };
-
+    }
 }
 
-export default new TicketsService();
+const ticketsServiceInstance = new TicketsService();
+
+export default ticketsServiceInstance;
